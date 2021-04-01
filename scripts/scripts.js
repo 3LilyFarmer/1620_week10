cfdcfd876e8d8295aa464f628ca4b956f05de35c
Michael_Chen <mmx30714@gmail.com>
2021-04-01 08:26:56 -0700

    add break, change console.log to alert

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,7 +8,8 @@ let user_input;
 do {
     user_input = prompt('Enter a 4 digit course code: ')
 } while(isNaN(user_input) === true);
-
+// alert(typeof(user_input))
+// alert(user_input)
 for(let item of courseList){
     if(item.code.includes(user_input)){
         alert(`Yes I am taking the course: ${item.code}: ${item.course_name}`);
@@ -16,6 +17,14 @@ for(let item of courseList){
     }
     else if(!item.code.includes(user_input)){
         courseList.push({code: user_input, course_name: null});
-        console.log(`${user_input} has been added to course list`);
+        alert(`Course ${user_input} has been added to course list`);
+        break;
     }
 }
+
+// for(let item of courseList){
+//     console.log(item.code.includes(1515))
+//     console.log(item.code.includes(1620))
+//     console.log(item.code.includes(1630))
+//     console.log(item.code.includes(2000))
+// }
